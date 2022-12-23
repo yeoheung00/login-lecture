d7dca4b39ab97a32a6d343461655708e1edd1cef
민경원 <min-kyeongwon@mingyeong-won-ui-noteubug.local>
2022-12-23 18:55:55 +0900

    studied in 2212231855

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,24 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+const id = document.querySelector("#id");
+const pw = document.querySelector("#pw");
+const loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login(){
+    const req = {
+        id: id.value,
+        pw: pw.value,
+    };
+
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req),
+    })
+    .then((res) => res.json())
+    .then(console.log);
+}
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,42 @@
 "use strict";
 
-const home = (req, res) => {
-    res.render("home/index");
+const output = {
+    home: (req, res) => {
+        res.render("home/index");
+    },
+
+    login: (req, res) => {
+        res.render("home/login");
+    },
+}
+
+const users = {
+    id: ["minnie", "miyeon", "soyeon", "yuqi", "shuhua"],
+    pw: ["1023", "0131", "0826", "0923", "0106"],
 };
 
-const login = (req, res) => {
-    res.render("home/login");
-};
+const process = {
+    login: (req, res) => {
+        const id = req.body.id;
+        const pw = req.body.pw;
+        console.log(id + ":" + pw);
+
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.pw[idx] === pw) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+        return res.json({
+            success: true,
+            msg: "로그인에 실패했습니다.",
+        });
+    },
+}
 
 module.exports = {
-    home,
-    login,
+    output,
+    process,
 };
