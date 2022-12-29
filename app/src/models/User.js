3c715f2d2db79fda60df85c42a7f162dc53d19f3
민경원 <min-kyeongwon@mingyeong-won-ui-noteubug.local>
2022-12-29 19:14:49 +0900

    백엔드 맛보기 최종 최적화

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -10,9 +10,9 @@ class User {
     async login() {
         const client = this.body;
         try {
-            const { id, pw } = await UserStorage.getUserInfo(client.id);
-            if (id) {
-                if (id === client.id && pw === client.pw) {
+            const user = await UserStorage.getUserInfo(client.id);
+            if (user) {
+                if (user.id === client.id && user.pw === client.pw) {
                     return { success: true };
                 }
                 return { success: false, msg: "비밀번호가 틀렸습니다." };
--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -5,7 +5,7 @@ const db = require("../config/db");
 class UserStorage {
     static getUserInfo(id) {
         return new Promise((resolve, reject) => {
-            const query = "SELECT * FROM asdf WHERE id = ?";
+            const query = "SELECT * FROM users WHERE id = ?";
             db.query(query, [id], (err, data) => {
                 if(err) reject(`${err}`);
                 else resolve(data[0]);
